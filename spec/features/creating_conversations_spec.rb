8642dfe1cd140f6aa25abd2c5d90ad4704f9974a
Finn Woelm <finn.woelm@gmail.com>
2016-05-11 02:29:17 +0200

    Completed list of specs

--- spec/features/creating_conversations_spec.rb
+++ spec/features/creating_conversations_spec.rb
@@ -1,5 +1,95 @@
 require 'rails_helper.rb'
 
+##################
+## ACCOUNTS
+##################
+feature 'accounts' do
+  
+  scenario 'cannot create an unpermitted account' do
+    
+    email_to_test = "abc@abc.com"
+    
+    # make sure email is not permtited
+    expect(OurnaropaForum::PermittedUser.find_by_email(email_to_test)).to be_nil
+    
+    visit '/forum'
+    click_link 'Sign Up'
+    fill_in 'Email', with: email_to_test
+    click_button "Ok"
+      
+    expect(page).to have_content("Sorry, email does not exist. Please contact the Admissions Office if you believe this is a mistake.")
+      
+  end
+  
+  scenario 'can create an account' do
+
+    @permitted_user = OurnaropaForum::PermittedUser.create! FactoryGirl.attributes_for(:ournaropa_forum_permitted_user)
+    
+    # make sure email is not permtited
+    expect(OurnaropaForum::PermittedUsers.find_by_email(@permitted_user.email)).to be_present
+    expect(OurnaropaForum::User.count).to eq(0)
+    
+    visit '/forum'
+    click_link 'Sign Up'
+    fill_in 'Email', with: @permitted_user.email
+    click_button "Ok"
+    
+    expect(page).to have_content("Hi")
+    expect(page).to have_content(@permitted_user.name)
+      
+      
+    click_button "Yup, that's me"
+      
+    # create new account
+    expect(OurnaropaForum::User.count).to eq(1)
+    
+  end
+    
+  scenario "can log in" do
+    pending
+  end
+  
+  scenario "can reset password" do
+    pending
+  end
+end
+
+##################
+## SUBSCRIPTIONS
+##################
+feature 'Subscriptions:' do
+  scenario 'subscribe to topic' do
+    pending
+  end
+  
+  scenario 'unsubscribe from topic' do
+    pending
+  end
+  
+  scenario 'unsubscribe from all' do
+    pending
+  end
+  
+  scenario 'new replies IF SUBSCRIBED' do
+    pending "expect sending of an email"
+  end
+  
+  scenario 'new replies IF UNSUBSCRIBED' do
+    pending "expect not sending of an email"
+  end
+  
+  scenario 'user not logged in for a week IF SUBSCRIBED' do
+    pending "expect sending of an email"
+  end
+  
+  scenario 'user not logged in for a week IF UNSUBSCRIBED' do
+    pending "expect not sending of an email"
+  end
+end
+  
+##################
+## TOPICS AND REPLIES
+##################
 feature 'Creating conversations' do  
   scenario 'can create a conversation' do
     
@@ -15,8 +105,7 @@ feature 'Creating conversations' do
     expect(page).to have_content(title)
     expect(page).to have_content(body)
   end
-    
-end  
+end
 
 feature 'Creating replies' do  
   scenario 'can create a reply' do
@@ -39,6 +128,9 @@ feature 'Creating replies' do
     
 end  
 
+##################
+## SETTINGS
+##################
 feature 'Editing Settings' do  
   scenario 'can see settings' do
       
@@ -64,6 +156,9 @@ feature 'Editing Settings' do
     
 end
 
+##################
+## PROFILES
+##################
 feature 'View Profile' do  
   scenario 'can view profile' do
       
@@ -75,6 +170,26 @@ feature 'View Profile' do
     
     expect(page).to have_content("Name")
     expect(page).to have_content("Bio")
+  end
+    
+end
+
+
+##################
+## DIRECTORY
+##################
+feature 'View Directory' do  
+  scenario 'can view directory' do
+    
+    10.times do
+      OurnaropaForum::User.create! FactoryGirl.attributes_for(:ournaropa_forum_user)
+    end
+      
+    visit '/forum'
+    click_link "Directory"
+    
+    expect(page).to have_content("Directory")
+    expect(page).to have_content("user-10") # 10 users
   end
     
 end
