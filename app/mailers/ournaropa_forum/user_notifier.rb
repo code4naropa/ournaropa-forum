af9618de72d45022943a7bf5d21f5080787e4725
Finn Woelm <finn.woelm@gmail.com>
2016-06-07 22:14:54 +0200

    Removed OurNaropa from emails; changed style of staff name badges

--- app/mailers/ournaropa_forum/user_notifier.rb
+++ app/mailers/ournaropa_forum/user_notifier.rb
@@ -1,130 +1,129 @@
 module OurnaropaForum
-  
+
   class UserNotifier < ActionMailer::Base
-        
+
     include Roadie::Rails::Automatic
-    
+
     add_template_helper(ApplicationHelper)
     include ApplicationHelper
-        
+
     layout '/ournaropa_forum/email'
-    
+
     default :from => "The Cushion <forum@thecushion.org>"
-        
+
     # invitation email
     def send_invitation_email(new_user, invited_by_user)
-      
+
       # initialize vars for email
       init_vars
       @new_user = new_user
       @invited_by_user = invited_by_user
-      
+
       # set footer
       @footer = "You received this email because #{@invited_by_user.first_name} #{@invited_by_user.last_name} invited you to join The Cushion."
-      
+
       mail( :to => "#{@new_user.first_name} #{@new_user.last_name} <#{@new_user.email}>",
         :subject => 'You are invited to join The Cushion' )
-      
+
     end
-    
+
     # send a signup email to the user, pass in the user object that   contains the user's email address
     def send_signup_email(user)
-      
+
       # initialize vars for email
       init_vars
       @user = user
-      
+
       # set footer
       @footer = 'You received this email because you registered an account.'
-      
+
       mail( :to => "#{@user.first_name} #{@user.last_name} <#{@user.email}>",
         :subject => 'Welcome to The Cushion' )
     end
-    
+
     def send_password_reset_email(user)
-      
+
       # initialize colors for email
       init_vars
-      
+
       @user = user
-      
+
       # set footer
       @footer = 'You received this email because someone triggered a password reset for your account.'
-            
+
       mail :to => "#{@user.first_name} #{@user.last_name} <#{@user.email}>",
       :subject => '[The Cushion] Password Recovery'
-    end     
-      
+    end
+
     def send_new_reply_email(user, reply)
-      
+
       # initialize colors for email
       init_vars
-      
+
       @user = user
       @reply = reply
-      
+
       @footer = "You received this email because email notifications for this conversation are enabled. *UNSUBSCRIBE_LINK*."
 
-      
+
       mail( :to => "#{@user.first_name} #{@user.last_name} <#{@user.email}>",
         :subject => "[The Cushion] New Reply in #{@reply.conversation.title}" )
-          
+
     end
 
     def send_inactivity_email(user, conversation_ids)
-      
+
       # initialize colors for email
-      init_vars     
+      init_vars
       @user = user
       @conversations = []
-      
+
       conversation_ids.each do |id|
         @conversations << Conversation.find(id)
       end
-            
+
       @footer = "You received this email because email notifications for inactivity are enabled. *UNSUBSCRIBE_LINK*."
-      
+
       mail( :to => "#{@user.first_name} #{@user.last_name} <#{@user.email}>",
         :subject => "[The Cushion] It's been a while!" )
-                
+
     end
-    
+
     # sends an email to the administrator summarizing the task controller action
     def send_task_summary_email(user, user_ids)
-      
+
       # initialize variables for email
-      init_vars     
+      init_vars
       @user = user
       @users = []
-      
+
       user_ids.each do |id|
         @users << User.find(id)
       end
-            
+
       @footer = "This is an automated email sent by The Cushion's Task Scheduler."
-      
+
       mail( :to => "#{@user.first_name} #{@user.last_name} <#{@user.email}>",
         :subject => "[The Cushion] Inactivity Emails Were Sent Successfully" )
-                
+
     end
-    
-    
+
+
     private
 
     def init_vars
       @primary_color = "#785a86"
       @highlight_color = "#E18416"
       @faded_color = "#616161"
-      
+
       @image_host = ENV["OURNAROPA_ASSET_HOST"]
     end
-    
+
     def roadie_options
 
       super unless Rails.env.test?
-      
+
     end
-    
+
   end
 end
-
--- spec/mailers/previews/ournaropa_forum/user_notifier_preview.rb
+++ spec/mailers/previews/ournaropa_forum/user_notifier_preview.rb
@@ -1,49 +1,49 @@
 module OurnaropaForum
   # Preview all emails at http://localhost:3000/rails/mailers/user_notifier
   class UserNotifierPreview < ActionMailer::Preview
-    
+
     include ApplicationHelper
-    
+
     def invitation
       UserNotifier.send_invitation_email(PermittedUser.first, User.last)
     end
-    
+
     def welcome
       u = User.first
       u.reset_token = User.generate_reset_token
       UserNotifier.send_signup_email(u)
     end
-    
+
     def forgot_password
       u = User.first
       u.reset_token = User.generate_reset_token
       UserNotifier.send_password_reset_email(u)
     end
-    
+
     def new_reply
-      UserNotifier.send_new_reply_email(User.first, Reply.first)
+      UserNotifier.send_new_reply_email(User.first, Conversation.first.replies.last)
     end
-    
+
     def inactivity_email
-      
+
       conversations = OurnaropaForum::Conversation.where("created_at > ?", Time.now - 7.days).order(updated_at: :desc).limit(3)
-      
+
       UserNotifier.send_inactivity_email(User.first, conversations)
     end
-    
+
     def task_summary
-      
+
       developer = User.find_by is_developer: true
-      
+
       users = OurnaropaForum::User.where(:is_receiving_inactivity_email => true).where("seen_at IS NULL or seen_at < ?", Time.now - 7.days).where("inactivity_email_sent_at IS NULL or inactivity_email_sent_at < ?", Time.now - 7.days)
       users_emailed = []
-      
+
       users.each do |user|
         users_emailed << user.id
       end
-            
+
       UserNotifier.send_task_summary_email(developer, users_emailed)
     end
-    
+
   end
 end
